57f7502dc6f4898041bcfb98d5fdd8fc9e79813f
Cecilie <cecilie@brandt.net>
2020-09-25 08:41:10 +0200

    TextCrypto started

---
+++ src/dk/kea/TextCrypto.java
@@ -0,0 +1,26 @@
+package dk.kea;
+
+//Needs to encrypt a file
+
+public class TextCrypto implements ICrypto{
+
+    @Override
+    public String encrypt(String text) {
+        return null;
+    }
+
+    @Override
+    public String decrypt(String text) {
+        return null;
+    }
+
+    @Override
+    public int getShift() {
+        return 0;
+    }
+
+    @Override
+    public void setShift(int shift) {
+
+    }
+}
